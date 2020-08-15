90fda4315d82b1a802f7c0e78eff6aa48c450e33
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-15 16:34:18 +0200

    Java Encapsulation

---
+++ MyMain.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.javaEncapsulation;
+
+public class MyMain
+{
+    public static void main(String[] args)
+    {
+        MyPerson maPersonne = new MyPerson();
+        maPersonne.setPrenom("Franck");
+        System.out.println(maPersonne.getPrenom());
+    }
+
+
+}
---
+++ MyPerson.java
@@ -0,0 +1,15 @@
+package be.intecbrussel.javaEncapsulation;
+
+public class MyPerson
+{
+    private String prenom = "Franck";
+
+    public String getPrenom()
+    {
+        return prenom;
+    }
+    public void setPrenom(String prenom)
+    {
+        this.prenom = prenom;
+    }
+}
